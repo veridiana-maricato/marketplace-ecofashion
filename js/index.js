b59453b0976dce44643557f43653021e24b2ce63
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-11-10 13:38:53 -0300

    teste

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// var loginButton = document.querySelector('#loginButton')
+// var loginDiv = document.querySelector(".login-div")
+
+
+// loginButton.addEventListener('mouseenter', removeHidden);
+// loginDiv.addEventListener('mouseleave', addHidden);
+
+// function removeHidden(){
+//     document.querySelector('#hidden').classList.remove('hidden')
+// }
+// function addHidden(){
+//     document.querySelector('#hidden').classList.add('hidden')
+// }
