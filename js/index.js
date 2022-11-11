4e7ed5358780792087a651f21e8c7d17ff608da0
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-11-11 18:01:13 -0300

    pagina de login pronta, pagina de marcas layout pronto

--- js/index.js
+++ js/index.js
@@ -11,3 +11,36 @@
 // function addHidden(){
 //     document.querySelector('#hidden').classList.add('hidden')
 // }
+
+
+// LOGIN / SINGIN
+    var login = document.querySelector('#login_title_btn')
+    var singin = document.querySelector('#singin_title_btn')
+
+       
+    login.addEventListener('click', singinHidden)
+    singin.addEventListener('click', loginHidden)
+
+
+function singinHidden(){
+    document.querySelector('.singin').classList.add('hidden')
+    document.querySelector('.login').classList.remove('hidden')
+    
+    document.querySelector('.singin_title').classList.remove('current_title')
+    document.querySelector('.singin_title').classList.add('not_current_title')
+
+    document.querySelector('.login_title').classList.remove('not_current_title')
+    document.querySelector('.login_title').classList.add('current_title')
+}
+
+function loginHidden(){
+    document.querySelector('.login').classList.add('hidden')
+    document.querySelector('.singin').classList.remove('hidden')
+
+    document.querySelector('.singin_title').classList.remove('not_current_title')
+    document.querySelector('.singin_title').classList.add('current_title')
+
+    document.querySelector('.login_title').classList.add('not_current_title')
+    document.querySelector('.login_title').classList.remove('current_title')
+    
+    }
